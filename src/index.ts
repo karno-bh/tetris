b957545fca40aa7ae84f8cd68d9c80d60adf5326
karno-bh <sudo.xterm@gmail.com>
2020-12-29 13:20:51 +0200

    cleaning up to conform to the project structure

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-import * as _ from 'lodash';
-import printMe from './print';
+import * as _ from 'lodash';;
 import { fizzBuzz } from './fizzBuzz';
-// import './style.css'
 
 function component() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     // element.classList.add('hello');
-    btn.innerHTML = 'Click me and check the console';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    // btn.onclick = printMe;
  
     return element;
 }
