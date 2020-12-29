22e42499a5e2d09de365aa69cf9c251e9ff9ecf4
karno-bh <sudo.xterm@gmail.com>
2020-12-29 07:51:51 +0200

    passed output management of webpack guide

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    // entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/i,
+    //             use: ['style-loader', 'css-loader'],
+    //         },
+    //     ],
+    // },
+}
